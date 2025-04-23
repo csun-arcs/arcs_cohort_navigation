9fd6b778d07d7d163b5e9d79c9fd4a5a599786c0
Barry Ridge <barry.ridge@csun.edu>
2025-04-23 08:57:51 -0700

    Refactor to remove use_dwb arg

--- launch/navigation_bringup.launch.py
+++ launch/navigation_bringup.launch.py
@@ -4,7 +4,7 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
@@ -18,9 +18,6 @@ def generate_launch_description():
     )
     default_slam_params = os.path.join(
         get_package_share_directory(pkg_nav), "config", "slam_params.yaml"
-    )
-    default_nav2_dwb_stamped_params = os.path.join(
-        get_package_share_directory(pkg_nav), "config", "nav2_dwb_stamped_params.yaml"
     )
     default_nav2_params = os.path.join(
         get_package_share_directory(pkg_nav), "config", "nav2_mppi_stamped_params.yaml"
@@ -40,9 +37,6 @@ def generate_launch_description():
         "slam_params",
         default_value=default_slam_params,
         description="Path to the params file to load for the slam_toolbox package SLAM node.",
-    )
-    declare_use_dwb_params_arg = DeclareLaunchArgument(
-        "use_dwb", default_value="false", description="Use DWB controller plugin"
     )
     declare_nav2_params_arg = DeclareLaunchArgument(
         "nav2_params",
@@ -72,13 +66,12 @@ def generate_launch_description():
 
     # Launch configurations
     use_sim_time = LaunchConfiguration("use_sim_time")
-    ekf_params = LaunchConfiguration("ekf_params")
-    slam_params = LaunchConfiguration("slam_params")
-    use_dwb = LaunchConfiguration("use_dwb")
-    nav2_params = LaunchConfiguration("nav2_params")
     use_ekf = LaunchConfiguration("use_ekf")
     use_slam = LaunchConfiguration("use_slam")
     use_nav2 = LaunchConfiguration("use_nav2")
+    ekf_params = LaunchConfiguration("ekf_params")
+    slam_params = LaunchConfiguration("slam_params")
+    nav2_params = LaunchConfiguration("nav2_params")
     log_level = LaunchConfiguration("log_level")
 
     # robot_localization EKF node
@@ -122,17 +115,7 @@ def generate_launch_description():
             ]
         ),
         launch_arguments={
-            "params_file": PythonExpression(
-                [
-                    '"',
-                    default_nav2_dwb_stamped_params,
-                    '" if "',
-                    use_dwb,
-                    '" == "true" else "',
-                    nav2_params,
-                    '"',
-                ]
-            ),
+            "params_file": nav2_params,
             "use_sim_time": use_sim_time,
         }.items(),
         condition=IfCondition(use_nav2),
@@ -142,13 +125,12 @@ def generate_launch_description():
         [
             # Declare arguments
             declare_use_sim_time_arg,
-            declare_ekf_params_arg,
-            declare_slam_params_arg,
-            declare_use_dwb_params_arg,
-            declare_nav2_params_arg,
             declare_use_ekf_arg,
             declare_use_slam_arg,
             declare_use_nav2_arg,
+            declare_ekf_params_arg,
+            declare_slam_params_arg,
+            declare_nav2_params_arg,
             declare_log_level_arg,
             # Nodes
             ekf_node,
